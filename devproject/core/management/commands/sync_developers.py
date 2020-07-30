173e2b3b3624d575f2266a9f5d88ba62f2cfc8b5
Francis Brito <fr.br94@gmail.com>
2020-07-30 18:28:29 -0400

    feat(command): update sync developers command to support using existing developers as input

--- devproject/core/management/commands/sync_developers.py
+++ devproject/core/management/commands/sync_developers.py
@@ -1,8 +1,12 @@
+import sys
 import traceback
 
 from django.core.management.base import BaseCommand, CommandParser
 
+from devproject.core.selectors import get_registered_developers
 from devproject.core.services import sync_developer
+
+ERROR_CODE_MISSING_INPUT = 1
 
 
 class Command(BaseCommand):
@@ -10,9 +14,29 @@ class Command(BaseCommand):
     requires_migrations_checks = True
 
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("logins", nargs="+", type=str)
+        parser.add_argument("logins", nargs="*", type=str)
+        parser.add_argument(
+            "-r",
+            "--registered",
+            action="store_true",
+            help="use registered developers as input",
+        )
 
     def handle(self, *args, **options) -> None:
+        if options["registered"]:
+            options["logins"] = get_registered_developers().values_list(
+                "login", flat=True
+            )
+
+        if len(options["logins"]) == 0:
+            self.stdout.write(
+                self.style.ERROR(
+                    "either provide an space-separated list of logins or pass --registered flag"
+                )
+            )
+
+            sys.exit(ERROR_CODE_MISSING_INPUT)
+
         for login in options["logins"]:
             self.stdout.write(f"syncing developer '{login}' ... ", ending="")
 
