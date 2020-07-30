0a034823828c86b8462ec403b789d1ba0afa9330
Francis Brito <fr.br94@gmail.com>
2020-07-30 18:03:49 -0400

    feat(tasks): add tasks to sync all registered developers and repositories

--- devproject/core/tasks.py
+++ devproject/core/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 
-from devproject.core import services
+from devproject.core import selectors, services
 
 
 @shared_task
@@ -11,3 +11,15 @@ def sync_repository(full_name: str) -> None:
 @shared_task
 def sync_developer(login: str) -> None:
     services.sync_developer(login=login)
+
+
+@shared_task
+def sync_registered_developers() -> None:
+    for dev in selectors.get_registered_developers():
+        sync_developer.delay(login=dev.login)
+
+
+@shared_task
+def sync_registered_repositories() -> None:
+    for repo in selectors.get_registered_repositories():
+        sync_repository.delay(full_name=repo.full_name)
