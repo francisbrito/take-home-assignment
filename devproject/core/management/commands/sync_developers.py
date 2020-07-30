418d932e669b10bad27beae6254ababa325036ad
Francis Brito <fr.br94@gmail.com>
2020-07-30 10:51:59 -0400

    feat(command): add sync developers command

--- devproject/core/management/commands/sync_developers.py
+++ devproject/core/management/commands/sync_developers.py
@@ -1,10 +1,25 @@
-from django.core.management.base import BaseCommand
+import traceback
+
+from django.core.management.base import BaseCommand, CommandParser
+
+from devproject.core.services import sync_developer
 
 
 class Command(BaseCommand):
     help = "creates or updates local developer information from Github"
     requires_migrations_checks = True
 
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument("logins", nargs="+", type=str)
+
     def handle(self, *args, **options) -> None:
-        # TODO: implement this.
-        raise NotImplementedError
+        for login in options["logins"]:
+            self.stdout.write(f"syncing developer '{login}' ... ", ending="")
+
+            try:
+                sync_developer(login=login)
+
+                self.stdout.write(self.style.SUCCESS("SUCCESS"))
+            except Exception as e:  # noqa
+                self.stdout.write(self.style.ERROR("FAILED"))
+                self.stderr.write(traceback.format_exc())
